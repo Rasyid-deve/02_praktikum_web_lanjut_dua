3ba498152a66b10374c10535930d87d1277155af
Rasyid-deve <renaldirasyed@gmail.com>
2021-02-19 06:33:07 +0700

    02_praktikum_web_lanjut_dua

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function index () {
+        echo "NIM: 1941720177 <br> Nama: Rasyed Renaldi";
+    }
+}
